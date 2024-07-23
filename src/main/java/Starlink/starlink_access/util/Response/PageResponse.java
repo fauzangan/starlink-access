1c151ef4dc79cf00af3fe68d436eaaca347222a0
Muhammad Amirul <mirulid@gmail.com>
2024-07-23 14:56:49 +0700

    feat:specification

---
+++ src/main/java/Starlink/starlink_access/util/Response/PageResponse.java
@@ -0,0 +1,22 @@
+package Starlink.starlink_access.util.Response;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PageResponse<T> {
+    private List<T> content;
+    private Integer size;
+    private Integer totalPages;
+    private Integer page;
+
+    public PageResponse(Page<T> page) {
+        this.content = page.getContent();
+        this.size = page.getSize();
+        this.totalPages = page.getTotalPages();
+        this.page = page.getNumber();
+    }
+}
