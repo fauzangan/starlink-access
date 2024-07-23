7f0b19296bacf11442fd57368e189a6d5d98bc48
fauzangan <fauzan12356@gmail.com>
2024-07-23 13:45:57 +0700

    first commit

---
+++ src/main/java/Starlink/starlink_access/StarlinkAccessApplication.java
@@ -0,0 +1,13 @@
+package Starlink.starlink_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlinkAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlinkAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Starlink/starlink_access/StarlinkAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package Starlink.starlink_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarlinkAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
