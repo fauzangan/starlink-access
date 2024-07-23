4d95eafbc36cf3086eab441e2de629d74657dc63
Muhammad Amirul <mirulid@gmail.com>
2024-07-23 15:14:41 +0700

    add PCservice

--- src/main/java/Starlink/starlink_access/service/ProductCategorySevice.java
+++ src/main/java/Starlink/starlink_access/service/ProductCategorySevice.java
@@ -1,5 +1,13 @@
 package Starlink.starlink_access.service;
 
+import Starlink.starlink_access.model.ProductCategory;
+
+import java.util.List;
+
 public interface ProductCategorySevice {
+ProductCategory create(String name);
+List<ProductCategory> getAll();
+ProductCategory getOne(Long id);
+void delete(Long id);
 
 }
