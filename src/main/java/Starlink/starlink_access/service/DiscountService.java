b954bd0033dddd118d6dde7e24264c59be7cb983
rifkimkurniawansyah <rifkimohamadk@gmail.com>
2024-07-23 22:14:35 +0700

    feat: Adding controller for discount and editing discountserviceimpl

--- src/main/java/Starlink/starlink_access/service/DiscountService.java
+++ src/main/java/Starlink/starlink_access/service/DiscountService.java
@@ -5,9 +5,12 @@ import Starlink.starlink_access.model.Discount;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface DiscountService {
     DiscountDTO create(DiscountDTO discountDTO);
-    Page<DiscountDTO> getAll(Pageable pageable, DiscountDTO discountDTO);
+    Page<DiscountDTO> getAll(Pageable pageable, String name, Integer percentage);
+    DiscountDTO getById(Long id);
     DiscountDTO update(Long id, DiscountDTO discountDTO);
     void delete (Long id);
 }
--- src/main/java/Starlink/starlink_access/service/implementation/DiscountServiceImplementation.java
+++ src/main/java/Starlink/starlink_access/service/implementation/DiscountServiceImplementation.java
@@ -6,11 +6,14 @@ import Starlink.starlink_access.model.Discount;
 import Starlink.starlink_access.model.Transaction;
 import Starlink.starlink_access.repository.DiscountRepository;
 import Starlink.starlink_access.service.DiscountService;
+import Starlink.starlink_access.util.specification.DiscountSpesification;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.mapper.Mapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,18 +28,28 @@ public class DiscountServiceImplementation implements DiscountService {
     }
 
     @Override
-    public Page<DiscountDTO> getAll(Pageable pageable, DiscountDTO discountDTO) {
-        Page<Discount> discounts = discountRepository.findAll(pageable);
+    public Page<DiscountDTO> getAll(Pageable pageable, String name, Integer percentage) {
+        var specification = DiscountSpesification.discountSpecification(name, percentage);
+        Page<Discount> discounts = discountRepository.findAll(specification, pageable);
         return discounts.map(DiscountMapper::map);
+    }
+
+    @Override
+    public DiscountDTO getById(Long id){
+        Discount discount = discountRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Discount Not Found"));
+        return DiscountMapper.map(discount);
     }
 
     @Override
     public DiscountDTO update(Long id, DiscountDTO discountDTO) {
         Discount existingDiscount = discountRepository.findById(id)
                 .orElseThrow(()-> new RuntimeException("Discount Not Found"));
+
         existingDiscount.setName(discountDTO.getName());
         existingDiscount.setPercentage(discountDTO.getPercentage());
         Discount updatedDiscount = discountRepository.save(existingDiscount);
+
         return DiscountMapper.map(updatedDiscount);
     }
 
