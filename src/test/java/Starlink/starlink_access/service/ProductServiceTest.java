50a498fb8b965cb291fa1ca3c3a646217ca7475e
Muhammad Amirul <mirulid@gmail.com>
2024-07-24 11:16:19 +0700

    givenProductDTO_whenSave_thenSave

---
+++ src/test/java/Starlink/starlink_access/service/ProductServiceTest.java
@@ -0,0 +1,66 @@
+package Starlink.starlink_access.service;
+
+import Starlink.starlink_access.DTO.ProductCategoryDTO;
+import Starlink.starlink_access.DTO.ProductDTO;
+import Starlink.starlink_access.mapper.ProductMapper;
+import Starlink.starlink_access.model.Product;
+import Starlink.starlink_access.model.ProductCategory;
+import Starlink.starlink_access.repository.ProductRepository;
+import Starlink.starlink_access.service.implementation.ProductServiceImplement;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+@Slf4j
+public class ProductServiceTest {
+    @Mock
+    private ProductRepository productRepository;
+    @InjectMocks
+    private ProductServiceImplement productSevice;
+
+
+    private ProductDTO productDTO;
+    private ProductCategoryDTO productCategoryDTO;
+
+    @BeforeEach
+    public void setUp() {
+        productCategoryDTO = ProductCategoryDTO.builder()
+                .name("High Speed")
+                .id(1L)
+                .build();
+        productDTO = ProductDTO.builder()
+                .id(1L)
+                .picture_source("https://")
+                .price(213254634L)
+                .stock(20L)
+                .name("always connect")
+                .productCategory(productCategoryDTO)
+                .build();
+    }
+
+    @DisplayName("JUnit test for Create Product")
+    @Test
+    public void givenProductDTO_whenSave_thenSave() {
+        given(productRepository.save(Mockito.any(Product.class))).willReturn(any(Product.class));
+
+        Product product = ProductMapper.map(productSevice.create(productDTO));
+        assertNotNull(product);
+        verify(productRepository, times(1)).save(any(Product.class));
+    }
+}
