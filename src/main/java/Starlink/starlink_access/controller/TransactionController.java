d6998e20e4ee6027d4c0f848d25aa83a02696066
fauzangan <fauzan12356@gmail.com>
2024-07-26 15:33:13 +0700

    Change the transaction request mapping

--- src/main/java/Starlink/starlink_access/controller/TransactionController.java
+++ src/main/java/Starlink/starlink_access/controller/TransactionController.java
@@ -11,7 +11,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/transaction")
+@RequestMapping("/api/transactions")
 @RequiredArgsConstructor
 public class TransactionController {
     private final TransactionService transactionService;
